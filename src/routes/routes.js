422169daa05f2d94840745900e655aa264568ce7
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-12-10 00:40:08 +0300

    navbar started

---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Home";
+import Main from "../layout/Main";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+        ]
+    }
+])
